83ad490602ef67c22fd4de0dea1be2a9723a6761
Gergely Papp <gpapp@hortonworks.com>
2018-11-08 21:41:41 +0100

    Refactor of AwsResourceConnector: downscale moved to AwsDownscaleService

--- cloud-aws/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/AwsResourceConnector.java
+++ cloud-aws/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/AwsResourceConnector.java
@@ -1,6 +1,5 @@
 package com.sequenceiq.cloudbreak.cloud.aws;
 
-import static com.sequenceiq.cloudbreak.cloud.aws.AwsInstanceConnector.INSTANCE_NOT_FOUND_ERROR_CODE;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
@@ -9,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -22,11 +20,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.autoscaling.model.DetachInstancesRequest;
-import com.amazonaws.services.autoscaling.model.UpdateAutoScalingGroupRequest;
-import com.amazonaws.services.ec2.AmazonEC2Client;
-import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 import com.sequenceiq.cloudbreak.api.model.AdjustmentType;
 import com.sequenceiq.cloudbreak.cloud.ResourceConnector;
 import com.sequenceiq.cloudbreak.cloud.aws.client.AmazonAutoScalingRetryClient;
@@ -36,7 +29,6 @@ import com.sequenceiq.cloudbreak.cloud.aws.encryption.EncryptedImageCopyService;
 import com.sequenceiq.cloudbreak.cloud.aws.encryption.EncryptedSnapshotService;
 import com.sequenceiq.cloudbreak.cloud.aws.scheduler.AwsBackoffSyncPollingScheduler;
 import com.sequenceiq.cloudbreak.cloud.aws.task.AwsPollTaskFactory;
-import com.sequenceiq.cloudbreak.cloud.aws.view.AwsCredentialView;
 import com.sequenceiq.cloudbreak.cloud.aws.view.AwsNetworkView;
 import com.sequenceiq.cloudbreak.cloud.context.AuthenticatedContext;
 import com.sequenceiq.cloudbreak.cloud.context.CloudContext;
@@ -46,7 +38,6 @@ import com.sequenceiq.cloudbreak.cloud.model.CloudResource;
 import com.sequenceiq.cloudbreak.cloud.model.CloudResourceStatus;
 import com.sequenceiq.cloudbreak.cloud.model.CloudStack;
 import com.sequenceiq.cloudbreak.cloud.model.Group;
-import com.sequenceiq.cloudbreak.cloud.model.InstanceStatus;
 import com.sequenceiq.cloudbreak.cloud.model.Network;
 import com.sequenceiq.cloudbreak.cloud.model.ResourceStatus;
 import com.sequenceiq.cloudbreak.cloud.model.TlsInfo;
@@ -149,6 +140,9 @@ public class AwsResourceConnector implements ResourceConnector<Object> {
 
     @Inject
     private AwsUpscaleService awsUpscaleService;
+
+    @Inject
+    private AwsDownscaleService awsDownscaleService;
 
     @Override
     public List<CloudResourceStatus> launch(AuthenticatedContext ac, CloudStack stack, PersistenceNotifier resourceNotifier,
@@ -255,70 +249,7 @@ public class AwsResourceConnector implements ResourceConnector<Object> {
     @Override
     public List<CloudResourceStatus> downscale(AuthenticatedContext auth, CloudStack stack, List<CloudResource> resources, List<CloudInstance> vms,
             Object resourcesToRemove) {
-        if (!vms.isEmpty()) {
-            List<String> instanceIds = new ArrayList<>();
-            for (CloudInstance vm : vms) {
-                instanceIds.add(vm.getInstanceId());
-            }
-
-            List<CloudResource> resourcesToDownscale = resources.stream()
-                    .filter(resource -> instanceIds.contains(resource.getInstanceId()))
-                    .collect(Collectors.toList());
-            awsComputeResourceService.deleteComputeResources(auth, stack, resourcesToDownscale);
-
-            String asGroupName = cfStackUtil.getAutoscalingGroupName(auth, vms.get(0).getTemplate().getGroupName(),
-                    auth.getCloudContext().getLocation().getRegion().value());
-            DetachInstancesRequest detachInstancesRequest = new DetachInstancesRequest().withAutoScalingGroupName(asGroupName).withInstanceIds(instanceIds)
-                    .withShouldDecrementDesiredCapacity(true);
-            AmazonAutoScalingRetryClient amazonASClient = awsClient.createAutoScalingRetryClient(new AwsCredentialView(auth.getCloudCredential()),
-                    auth.getCloudContext().getLocation().getRegion().value());
-            detachInstances(instanceIds, detachInstancesRequest, amazonASClient);
-            AmazonEC2Client amazonEC2Client = awsClient.createAccess(new AwsCredentialView(auth.getCloudCredential()),
-                    auth.getCloudContext().getLocation().getRegion().value());
-            terminateInstances(instanceIds, amazonEC2Client);
-            LOGGER.info("Terminated instances in stack '{}': '{}'", auth.getCloudContext().getId(), instanceIds);
-            try {
-                amazonASClient.updateAutoScalingGroup(new UpdateAutoScalingGroupRequest()
-                        .withAutoScalingGroupName(asGroupName)
-                        .withMaxSize(getInstanceCount(stack, vms.get(0).getTemplate().getGroupName())));
-            } catch (AmazonServiceException e) {
-                LOGGER.warn(e.getErrorMessage());
-            }
-        }
-        return check(auth, resources);
-    }
-
-    private void terminateInstances(List<String> instanceIds, AmazonEC2Client amazonEC2Client) {
-        try {
-            amazonEC2Client.terminateInstances(new TerminateInstancesRequest().withInstanceIds(instanceIds));
-        } catch (AmazonServiceException e) {
-            if (!INSTANCE_NOT_FOUND_ERROR_CODE.equals(e.getErrorCode())) {
-                throw e;
-            }
-            LOGGER.info(e.getErrorMessage());
-        }
-    }
-
-    private void detachInstances(List<String> instanceIds, DetachInstancesRequest detachInstancesRequest, AmazonAutoScalingRetryClient amazonASClient) {
-        try {
-            amazonASClient.detachInstances(detachInstancesRequest);
-        } catch (AmazonServiceException e) {
-            if (!"ValidationError".equals(e.getErrorCode())
-                    || !e.getErrorMessage().contains("not part of Auto Scaling")
-                    || instanceIds.stream().anyMatch(id -> !e.getErrorMessage().contains(id))) {
-                throw e;
-            }
-            LOGGER.info(e.getErrorMessage());
-        }
-    }
-
-    private int getInstanceCount(CloudStack stack, String groupName) {
-        int result = -1;
-        Optional<Group> group = stack.getGroups().stream().filter(g -> g.getName().equals(groupName)).findFirst();
-        if (group.isPresent()) {
-            result = (int) group.get().getInstances().stream().filter(inst -> !inst.getTemplate().getStatus().equals(InstanceStatus.DELETE_REQUESTED)).count();
-        }
-        return result;
+        return awsDownscaleService.downscale(auth, stack, resources, vms, resourcesToRemove);
     }
 
     @Override

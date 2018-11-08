35451a458783cdd5c6109c7be6c4fbe2fac08011
Gergely Papp <gpapp@hortonworks.com>
2018-11-08 13:27:54 +0100

    mount-disk: disk is only formatted if it has no fs yet

--- orchestrator-salt/src/main/resources/salt/salt/disk/mount-disks.sh
+++ orchestrator-salt/src/main/resources/salt/salt/disk/mount-disks.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
 format_disks() {
   lazy_format_disks
   cd /hadoopfs/fs1 && mkdir logs logs/ambari-server logs/ambari-agent logs/consul-watch logs/kerberos
 }
 
 lazy_format_disks() {
+  FS_TYPE=ext4
   mkdir /hadoopfs
   for (( i=1; i<=24; i++ )); do
     LABEL=$(printf "\x$(printf %x $((START_LABEL+i)))")
@@ -16,9 +16,12 @@ lazy_format_disks() {
         umount "$MOUNTPOINT"
         sed -i "\|^$DEVICE|d" /etc/fstab
       fi
-      mkfs -E lazy_itable_init=1 -O uninit_bg -F -t ext4 $DEVICE
+      if [ -z "$(blkid $DEVICE | grep $FS_TYPE)" ]; then
+          echo "formatting: $DEVICE"
+          mkfs -E lazy_itable_init=1 -O uninit_bg -F -t $FS_TYPE $DEVICE
+      fi
       mkdir /hadoopfs/fs${i}
-      echo UUID=$(blkid -o value $DEVICE | head -1) /hadoopfs/fs${i} ext4  defaults,noatime,nofail 0 2 >> /etc/fstab
+      echo UUID=$(blkid -o value $DEVICE | head -1) /hadoopfs/fs${i} $FS_TYPE  defaults,noatime,nofail 0 2 >> /etc/fstab
       mount /hadoopfs/fs${i}
       chmod 777 /hadoopfs/fs${i}
     fi
